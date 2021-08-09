66a25219e73be86ca0123bf1c02758e0a72e3626
Jonathan Muñoz <jonathan2712emr@gmail.com>
2021-08-09 12:09:47 -0500

    Author form created, project barebones ready
    Author form module created and ready to implement functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthorsListComponent } from './authors-list/authors-list.component';
+import { AuthorFormComponent } from './author-form/author-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthorsListComponent,
+    AuthorFormComponent
   ],
   imports: [
     BrowserModule,
